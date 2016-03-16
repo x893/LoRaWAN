e96ca87e9944946134ac1f2a2607b9cc7c198564
x893 <x893.00@gmail.com>
2016-03-16 20:55:42 +0300

    Add TTN tests, simple UdpServer.

---
+++ TTNClient/IoTServer.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+using Newtonsoft.Json;
+using IoT.Protocol;
+
+namespace IoT
+{
+	public enum PACKET_TYPE : byte
+	{
+		PKT_PUSH_DATA = 0,
+		PKT_PUSH_ACK = 1,
+		PKT_PULL_DATA = 2,
+		PKT_PULL_RESP = 3,
+		PKT_PULL_ACK = 4,
+		PKT_TX_ACK = 5,
+		PKT_UNKNOWN
+	}
+
+	public enum SERVER_STATE
+	{
+		Initialize,
+		Running,
+		Stoped
+	}
+
+	public class GatewayState
+	{
+		public UInt64 EUI;
+		public DateTime LastTime;
+		public EndPoint EndPoint;
+		public GatewayState()
+		{ }
+
+		public override string ToString()
+		{
+			return (string.Format(
+				"{{Time:{0}, EUI:{1:X8}, Remote:{2}}}",
+				LastTime,
+				EUI,
+				EndPoint
+				));
+		}
+	}
+
+	public enum PULL_RESP_STATE
+	{
+		Initialize,
+		Ready,
+		Sended
+	}
+	public class GatewayPullResp
+	{
+		public PULL_RESP_STATE State;
+		public DateTime TimeToSend;
+		public string Error;
+		public int Retry;
+		public GatewayTxpkV1 Txpk;
+		public GatewayPullResp()
+		{
+			State = PULL_RESP_STATE.Initialize;
+		}
+	}
+
+	public class ServerState
+	{
+		public UInt32 PullRespToken;
+		public Dictionary<UInt32, GatewayPullResp> PullRespPackets;
+		public object LockPullResp;
+		public Socket Socket;
+		public SERVER_STATE State;
+		public Queue<GatewayPacket> Packets;
+		public object LockPackets;
+		public int LostPackets;
+		public Exception ErrorSendToCB;
+		public Exception ErrorSendTo;
+		public Exception ErrorReceiveFromCB;
+		public Exception ErrorReceiveFrom;
+		public Dictionary<UInt64, GatewayState> Gateways;
+		public byte[] Buffer = new byte[2500];
+		public ManualResetEvent ResetEvent;
+
+		public ServerState()
+		{
+			State = SERVER_STATE.Initialize;
+			LockPackets = new object();
+			LockPullResp = new object();
+			PullRespToken = 0;
+			PullRespPackets = new Dictionary<uint, GatewayPullResp>(100);
+			Packets = new Queue<GatewayPacket>(100);
+			Gateways = new Dictionary<ulong, GatewayState>(100);
+			ResetEvent = new ManualResetEvent(false);
+		}
+	}
+
+	public class IoTServer
+	{
+		public ServerState state = new ServerState();
+
+		public IoTServer()
+		{
+		}
+
+		public void Run()
+		{
+			Console.WriteLine("\nPress any key to exit");
+
+			JsonSerializerSettings settings = new JsonSerializerSettings();
+			settings.Culture = CultureInfo.InvariantCulture;
+
+			try
+			{
+				state.Socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+				state.Socket.Bind(new IPEndPoint(IPAddress.Any, Properties.Settings.Default.LwgPort));
+
+				IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 0);
+				EndPoint epSender = (EndPoint)ipep;
+				state.Socket.BeginReceiveFrom(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, ref epSender, new AsyncCallback(ReceiveFromCallback), state);
+				state.State = SERVER_STATE.Running;
+
+				/*
+				 * Test PULL_RESP
+				 */
+				GatewayPullResp TestPullRest = new GatewayPullResp();
+				{
+					GatewayTxpkV1 txpk = new GatewayTxpkV1();
+					txpk.imme = true;
+					txpk.tmst = 0;
+					txpk.time = DateTime.UtcNow;
+					txpk.freq = 864.123456;
+					txpk.rfch = 0;
+					txpk.powe = 14;
+					txpk.modu = "SF11BW125";
+					txpk.codr = "4/6";
+					txpk.ipol = false;
+					txpk.size = 32;
+					txpk.data = "H3P3N2i9qc4yt7rK7ldqoeCVJGBybzPY5h1Dd7P7p8v";
+
+					TestPullRest.Txpk = txpk;
+				}
+
+				DateTime keyTime = DateTime.Now;
+				while (state.State == SERVER_STATE.Running)
+				{
+					while (state.Packets.Count > 0)
+					{
+						#region Process packets
+						GatewayPacket packet = null;
+						lock (state.LockPackets)
+						{
+							packet = state.Packets.Dequeue();
+							state.ResetEvent.Reset();
+						}
+
+						if (packet == null)
+							continue;
+
+						Console.WriteLine(string.Format("Recv from {0}", packet.Sender));
+						Console.WriteLine(packet.ToString());
+
+						try
+						{
+							switch (packet.PacketType)
+							{
+								case PACKET_TYPE.PKT_TX_ACK:
+									UInt32 token = packet.Token;
+									GatewayTxAck data =
+										string.IsNullOrEmpty(packet.Json)
+										? null
+										: JsonConvert.DeserializeObject<GatewayTxAck>(packet.Json, settings);
+
+									if (data != null)
+									{	// Error
+										Console.WriteLine("ERROR:");
+										Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+										if (state.PullRespPackets.ContainsKey(token))
+										{
+											lock (state.LockPullResp)
+											{
+												GatewayPullResp pull = state.PullRespPackets[token];
+												if (pull.Retry > 0)
+												{
+													pull.Retry--;
+													pull.TimeToSend = DateTime.Now.AddSeconds(15);
+													pull.State = PULL_RESP_STATE.Ready;
+												}
+												else
+												{
+													state.PullRespPackets.Remove(token);
+												}
+											}
+										}
+									}
+									else
+									{	// No error
+										if (state.PullRespPackets.ContainsKey(token))
+											lock (state.LockPullResp)
+												state.PullRespPackets.Remove(token);
+									}
+									break;
+
+								case PACKET_TYPE.PKT_PULL_DATA:
+									GatewayState gw;
+									UInt64 eui = BitConverter.ToUInt64(packet.EUI, 0);
+									if (state.Gateways.ContainsKey(eui))
+										gw = state.Gateways[eui];
+									else
+										gw = new GatewayState();
+
+									gw.EndPoint = packet.Sender;
+									gw.LastTime = DateTime.Now;
+									SendTo(state, packet);
+									break;
+
+								case PACKET_TYPE.PKT_PUSH_DATA:
+									if (string.IsNullOrEmpty(packet.Json))
+									{
+										Console.WriteLine("JSON ERROR: Empty payload");
+									}
+									else
+									{
+										GatewayPushData json =
+											(packet.Version == 1)
+											? (GatewayPushData)JsonConvert.DeserializeObject<GatewayPushDataV1>(packet.Json, settings)
+											: (GatewayPushData)JsonConvert.DeserializeObject<GatewayPushDataV2>(packet.Json, settings)
+											;
+										if (json != null)
+										{
+											/*
+											Console.WriteLine("JSON:");
+											Console.WriteLine(JsonConvert.SerializeObject(json, Formatting.Indented));
+											*/
+											bool success = true;
+											if (json.stat != null)
+												success &= ProcessingStat(json.stat);
+
+											if (json.rxpk != null && json.rxpk.Count > 0)
+												for (int idx = 0; idx < json.rxpk.Count; idx++)
+													success &= ProcessingRxpt(json.rxpk[idx]);
+											if (success)
+											{
+												// Thread.Sleep(10);
+												SendTo(state, packet);
+											}
+										}
+									}
+									break;
+								default:
+									break;
+							}
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(string.Format("Processing error:\n{0}", ex.ToString()));
+						}
+						#endregion
+					}
+
+					lock (state.LockPackets)
+						if (state.Packets.Count == 0)
+							state.ResetEvent.Reset();
+
+					if (!state.ResetEvent.WaitOne(500))
+					{
+						if ((DateTime.Now - keyTime).Seconds >= 5)
+						{
+							keyTime = DateTime.Now;
+							if (Console.KeyAvailable)
+							{
+								while (Console.KeyAvailable)
+									Console.ReadKey(true);
+								state.State = SERVER_STATE.Stoped;
+								break;
+							}
+						}
+					}
+				}
+
+				state.Socket.Shutdown(SocketShutdown.Both);
+				state.Socket.Close();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+
+		private bool ProcessingRxpt(GatewayRxpk gatewayRxpk)
+		{
+			return true;
+		}
+
+		private bool ProcessingStat(GatewayStat gatewayStat)
+		{
+			return true;
+		}
+
+		private void ReceiveFromCallback(IAsyncResult ar)
+		{
+			ServerState state = (ServerState)ar.AsyncState;
+			if (state.State == SERVER_STATE.Running)
+			{
+				Socket socket = state.Socket;
+				try
+				{
+					// Read packet from gateway
+					IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 0);
+					EndPoint epSender = (EndPoint)ipep;
+					int bytesRead = socket.EndReceiveFrom(ar, ref epSender);
+					byte[] buffer = state.Buffer;
+
+					// Check packet from gateway
+					if (bytesRead >= 4)
+					{
+						byte version = buffer[0];
+						if (version == 1 || version == 2)
+						{
+							GatewayPacket packet = new GatewayPacket();
+							packet.Version = version;
+							packet.Token = BitConverter.ToUInt16(buffer, 1);
+
+							byte type = buffer[3];
+							switch (type)
+							{
+								case (byte)PACKET_TYPE.PKT_PUSH_DATA:
+									if (bytesRead > 12)
+									{
+										packet.Json = Encoding.ASCII.GetString(buffer, 12, bytesRead - 12);
+										packet.SetEUI(buffer);
+									}
+									else
+										packet = null;
+									break;
+								case (byte)PACKET_TYPE.PKT_PULL_DATA:
+									if (bytesRead == 12)
+										packet.SetEUI(buffer);
+									else
+										packet = null;
+									break;
+								case (byte)PACKET_TYPE.PKT_TX_ACK:
+									if (bytesRead > 4)
+										packet.Json = Encoding.ASCII.GetString(buffer, 4, bytesRead - 4);
+									else
+										packet = null;
+									break;
+								default:
+									packet = null;
+									break;
+							}
+
+							if (packet != null)
+							{
+								packet.PacketType = (PACKET_TYPE)type;
+								packet.Sender = epSender;
+
+								lock (state.LockPackets)
+								{
+									if (state.Packets.Count < 100)
+										state.Packets.Enqueue(packet);
+									else
+										state.LostPackets++;
+
+									state.ResetEvent.Set();
+								}
+							}
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					state.ErrorReceiveFromCB = ex;
+				}
+
+				try
+				{
+					IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 0);
+					EndPoint epSender = (EndPoint)ipep;
+					socket.BeginReceiveFrom(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, ref epSender, new AsyncCallback(ReceiveFromCallback), state);
+				}
+				catch(Exception ex)
+				{
+					state.ErrorReceiveFrom = ex;
+				}
+			}
+		}
+
+		private void SendTo(ServerState state, GatewayPacket packet)
+		{
+			if (state.State == SERVER_STATE.Running)
+			{
+				byte[] data = null;
+				PACKET_TYPE type = PACKET_TYPE.PKT_UNKNOWN;
+
+				switch (packet.PacketType)
+				{
+					case PACKET_TYPE.PKT_PUSH_DATA:
+						data = new byte[4];
+						type = PACKET_TYPE.PKT_PUSH_ACK;
+						break;
+					case PACKET_TYPE.PKT_PULL_DATA:
+						data = new byte[12];
+						packet.EUI.CopyTo(data, 4);
+						type = PACKET_TYPE.PKT_PULL_ACK;
+						break;
+					default:
+						break;
+				}
+
+				if (type != PACKET_TYPE.PKT_UNKNOWN)
+				{
+					data[0] = packet.Version;
+					BitConverter.GetBytes(packet.Token).CopyTo(data, 1);
+					data[3] = (byte)type;
+
+					try
+					{
+						state.Socket.BeginSendTo(data, 0, data.Length, SocketFlags.None, packet.Sender, new AsyncCallback(SendToCallback), state);
+					}
+					catch (Exception ex)
+					{
+						state.ErrorSendTo = ex;
+					}
+				}
+			}
+		}
+
+		private void SendToCallback(IAsyncResult ar)
+		{
+			ServerState state = (ServerState)ar.AsyncState;
+			if (state.State == SERVER_STATE.Running)
+			{
+				try
+				{
+					state.Socket.EndSendTo(ar);
+				}
+				catch (Exception ex)
+				{
+					state.ErrorSendToCB = ex;
+				}
+			}
+		}
+	}
+}
---
+++ TTNClient/Program.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+using RestSharp;
+using Newtonsoft.Json;
+using IoT.Protocol;
+
+
+namespace IoT
+{
+	class Program
+	{
+		static bool complete;
+
+		static void Main(string[] args)
+		{
+			UseMQTT();
+
+			// UseREST();
+
+			// IoTServer server = new IoTServer();
+			// server.Run();
+
+			WaitKeyPress();
+			Environment.Exit(0);
+		}
+
+		#region client_MqttMsgPublishReceived
+		static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+		{
+			Console.WriteLine(string.Format(
+				"------ PublishReceived\nTopic:{0} DupFlag:{1} QosLevel:{2} Retain:{3}",
+				e.Topic,
+				e.DupFlag,
+				e.QosLevel,
+				e.Retain
+				));
+
+			if (!string.IsNullOrEmpty(e.Topic) && e.Message != null && e.Message.Length > 0)
+			{
+				string msg = Encoding.UTF8.GetString(e.Message);
+				try
+				{
+					if (string.IsNullOrEmpty(msg))
+					{
+						Console.WriteLine("MESSAGE: EMPTY");
+					}
+					else if (e.Topic.StartsWith("gateways/"))
+					{
+						ApiGatewayStatus status = JsonConvert.DeserializeObject<ApiGatewayStatus>(msg);
+						Console.WriteLine(string.Format("STATUS: {0}", status));
+					}
+					else if (e.Topic.StartsWith("nodes/"))
+					{
+						ApiNodePacket packet = JsonConvert.DeserializeObject<ApiNodePacket>(msg);
+						Console.WriteLine(string.Format("PACKET: {0}", packet));
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(string.Format("JSON ERROR: {0}", ex.Message));
+					Console.WriteLine(string.Format("MESSAGE: {0}", msg));
+				}
+			}
+			else
+			{
+				Console.WriteLine("ERROR: EMPTY MESSAGE");
+			}
+		}
+		#endregion
+		#region client_MqttMsgUnsubscribed
+		static void client_MqttMsgUnsubscribed(object sender, MqttMsgUnsubscribedEventArgs e)
+		{
+			Console.WriteLine(string.Format(
+				"------ Unsubscribed MsgId:{0}", e.MessageId
+				));
+			complete = true;
+		}
+		#endregion
+		#region client_MqttMsgSubscribed
+		static void client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+		{
+			Console.WriteLine(string.Format(
+				"------ Subscribed MsgId:{0}", e.MessageId
+				));
+			complete = true;
+		}
+		#endregion
+		#region client_ConnectionClosed
+		static void client_ConnectionClosed(object sender, EventArgs e)
+		{
+			Console.WriteLine(string.Format(
+				"------ Connection Closed"
+				));
+			complete = true;
+		}
+		#endregion
+
+		#region UseMQTT()
+		private static void UseMQTT()
+		{
+			MqttClient mqtt = null;
+			try
+			{
+				// Create M2MQTT client
+				mqtt = new MqttClient(
+					Properties.Settings.Default.MQTTServer,	// Server name
+					Properties.Settings.Default.MQTTPort,	// Server port
+					false,
+					MqttSslProtocols.None,
+					null,
+					null
+					);
+				// Set protocol for server
+				mqtt.ProtocolVersion = MqttProtocolVersion.Version_3_1;
+				// Add event handlers
+				mqtt.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+				mqtt.MqttMsgSubscribed += client_MqttMsgSubscribed;
+				mqtt.MqttMsgUnsubscribed += client_MqttMsgUnsubscribed;
+				mqtt.ConnectionClosed += client_ConnectionClosed;
+
+				Console.WriteLine(string.Format(
+					"Connect to broker: {0}:{1}",
+					Properties.Settings.Default.MQTTServer,
+					Properties.Settings.Default.MQTTPort
+					));
+				// Subscribe to broker topics
+				// 1. gateway messages
+				// 2. nodes packets
+				string[] topic = new string[Properties.Settings.Default.MQTTTopic.Count];
+				byte[] qos = new byte[Properties.Settings.Default.MQTTTopic.Count];
+				for (int i = 0; i < topic.Length; i++)
+				{
+					topic[i] = Properties.Settings.Default.MQTTTopic[i];
+					Console.WriteLine(string.Format("Subscribe to {0}", topic[i]));
+					qos[i] = MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE;
+				}
+				// Connect to server
+				mqtt.Connect(Guid.NewGuid().ToString());
+				// and subscribe
+				ushort msgId = mqtt.Subscribe(topic, qos);
+
+
+				WaitKeyPress("Press any key to exit\n");
+
+				// Unsubscribe
+				mqtt.Unsubscribe(topic);
+				complete = false;
+				for (int timeout = 0; timeout < 500; timeout++)
+				{
+					if (complete)
+						break;
+					Thread.Sleep(10);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(string.Format("MQTT ERROR:{0}", ex.Message));
+			}
+
+			// Disconnect from server
+			if (mqtt != null && mqtt.IsConnected)
+			{
+				mqtt.Disconnect();
+				complete = false;
+				for (int timeout = 0; timeout < 500; timeout++)
+				{
+					if (complete)
+						break;
+					Thread.Sleep(10);
+				}
+			}
+
+			WaitKeyPress();
+		}
+		#endregion
+
+		#region UseREST()
+		private static void UseREST()
+		{
+			string server = Properties.Settings.Default.Server;
+			string serverFormat = Properties.Settings.Default.ServerFormat;
+			string gatewayUrl = Properties.Settings.Default.GatewayUrl;
+			string nodeUrl = Properties.Settings.Default.NodeUrl;
+			bool useDB = Properties.Settings.Default.UseDB;
+			int limit = Properties.Settings.Default.Limit;
+
+			Dictionary<string, Gateway> gw_list = new Dictionary<string, Gateway>();
+
+			try
+			{
+				if (useDB)
+				{
+					// Load gateways list from database
+					using (LgwDbContext ctx = new LgwDbContext())
+					{
+						List<Gateway> list = ctx.Database.SqlQuery<Gateway>(@"
+SET NOCOUNT ON
+SELECT * FROM [Gateway]
+").ToList();
+						gw_list = new Dictionary<string, Gateway>(list.Count);
+
+						foreach (Gateway gw in list)
+						{
+							if (gw_list.ContainsKey(gw.eui))
+							{	// Multiple gateway EUI found
+								Console.WriteLine(string.Format("Duplicate gateway {0} in database", gw.eui));
+								WaitKeyPress();
+								Environment.Exit(1);
+							}
+							gw_list.Add(gw.eui, gw);
+						}
+					}
+
+					/*
+					// Load nodes list from database
+					using (LgwDbContext ctx = new LgwDbContext())
+					{
+						List<Gateway> list = ctx.Database.SqlQuery<Gateway>(@"
+SET NOCOUNT ON
+SELECT * FROM [Gateway]
+").ToList();
+						gw_list = new Dictionary<string, Gateway>(list.Count);
+
+						foreach (Gateway gw in list)
+						{
+							if (gw_list.ContainsKey(gw.eui))
+							{	// Multiple gateway EUI found
+								Console.WriteLine(string.Format("Duplicate gateway {0} in database", gw.eui));
+								WaitKeyPress();
+								Environment.Exit(1);
+							}
+							gw_list.Add(gw.eui, gw);
+						}
+					}
+					*/
+				}
+
+				RestClient client = new RestClient(server);
+
+				foreach (string eui in Properties.Settings.Default.NodeEUI)
+				{
+					Console.WriteLine(string.Format("Last {0} messages for nodes {1}", limit == 0 ? "all" : limit.ToString(), eui));
+
+					RestRequest request = new RestRequest(nodeUrl, Method.GET);
+					if (!string.IsNullOrEmpty(serverFormat))
+						request.AddParameter("format", serverFormat);
+					if (limit > 0)
+						request.AddParameter("limit", limit);
+					request.AddUrlSegment("eui", eui);
+
+					IRestResponse<List<ApiNodePacket>> response = client.Execute<List<ApiNodePacket>>(request);
+
+					if (response.ResponseStatus == ResponseStatus.Completed && response.Data != null)
+					{
+						foreach (ApiNodePacket data in response.Data)
+						{
+							if (useDB)
+							{
+								DBCheckGateway(gw_list, data.gateway_eui);
+								// DBCheckNode(gw_list, status);
+							}
+							Console.WriteLine(data.ToString());
+						}
+					}
+					else
+					{
+						Console.WriteLine("Error:");
+						if (response.ErrorException != null)
+							Console.WriteLine(response.ErrorException);
+						Console.WriteLine(response.Content);
+						break;
+					}
+				}
+
+				foreach (string eui in Properties.Settings.Default.GatewayEUI)
+				{
+					Console.WriteLine(string.Format("Last {0} status messages for gateway {1}", limit == 0 ? "all" : limit.ToString(), eui));
+
+					RestRequest request = new RestRequest(gatewayUrl, Method.GET);
+					if (!string.IsNullOrEmpty(serverFormat))
+						request.AddParameter("format", serverFormat);
+					if (limit > 0)
+						request.AddParameter("limit", limit);
+					request.AddUrlSegment("eui", eui);
+
+					IRestResponse<List<ApiGatewayStatus>> response = client.Execute<List<ApiGatewayStatus>>(request);
+
+					if (response.ResponseStatus == ResponseStatus.Completed && response.Data != null)
+					{
+						foreach (ApiGatewayStatus status in response.Data)
+						{
+							if (useDB)
+								DBCheckGateway(gw_list, status);
+							Console.WriteLine(status.ToString());
+						}
+					}
+					else
+					{
+						Console.WriteLine("Error:");
+						if (response.ErrorException != null)
+							Console.WriteLine(response.ErrorException);
+						Console.WriteLine(response.Content);
+						break;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(string.Format("Error: {0}", ex.Message));
+				WaitKeyPress();
+				Environment.Exit(2);
+			}
+			WaitKeyPress();
+			Environment.Exit(0);
+		}
+		#endregion
+
+		#region Wait key with message
+		/// <summary>
+		/// 
+		/// </summary>
+		private static void WaitKeyPress()
+		{
+			WaitKeyPress("\nPress any key to continue ...");
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="msg"></param>
+		private static void WaitKeyPress(string msg)
+		{
+			Console.Write(msg);
+			Console.ReadKey(true);
+		}
+		#endregion
+
+		#region Check gateway in database
+		private static void DBCheckGateway(Dictionary<string, Gateway> gw_list, string p)
+		{
+
+		}
+
+		/// <summary>
+		/// Add gateway to database
+		/// </summary>
+		/// <param name="gw_db_list"></param>
+		/// <param name="status"></param>
+		private static void DBCheckGateway(Dictionary<string, Gateway> gw_db_list, ApiGatewayStatus status)
+		{
+			if (!gw_db_list.ContainsKey(status.eui))
+			{
+				try
+				{
+					Console.WriteLine(string.Format("Add gateway {0} to database", status.eui));
+
+					Gateway gw = new Gateway();
+					gw.eui = status.eui;
+					gw.latitude = status.latitude;
+					gw.longitude = status.longitude;
+					gw.time = status.time;
+					using (LgwDbContext ctx = new LgwDbContext())
+					{
+						gw_db_list.Add(gw.eui, gw);
+						ctx.Gateway.Add(gw);
+						ctx.SaveChanges();
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(string.Format("Error: {0}", ex.Message));
+					throw ex;
+				}
+			}
+		}
+		#endregion
+	}
+
+}
